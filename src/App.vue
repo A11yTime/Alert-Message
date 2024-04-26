04e6fa709ab5a7035e267c257d8d8f0098dd2e70
MD <m.sohel.sw@gmail.com>
2024-04-25 18:07:06 -0700

    alert

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AlertMessage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlertMessage from './components/AlertMessage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AlertMessage
   }
 }
 </script>
---
+++ src/components/AlertMessage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="border">
+    <h3>Role alert implementation for screen reader</h3>
+    <div>
+        <button class="button primary" @click="buttonClicked('Primary')">Primary <span aria-hidden="true" >Button</span></button>
+        <button class="button secondary" @click="buttonClicked('Secondary')">Secondary <span aria-hidden="true">Button</span></button>
+        <button class="button tertiary" @click="buttonClicked('Tertiary')">Tertiary <span aria-hidden="true">Button</span></button>
+        <p v-if="clickedButton" role="alert">You clicked the {{ clickedButton }} button.</p>
+    </div>
+  </div>
+</template>
+<script>
+export default{
+    data(){
+        return {
+            clickedButton: ''
+        }
+    },
+    methods: {
+      buttonClicked(type) {
+        this.clickedButton = type;
+      }
+    }
+}
+</script>
+<style scoped>
+  .border {
+    background-color: rgb(218, 245, 248);
+    padding: 10px;
+    margin: 20px;
+  }
+.button {
+  display: inline-block;
+  padding: 10px 20px;
+  margin: 5px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  transition: background-color 0.3s;
+}
+.primary {
+  background-color: #007bff;
+  color: #fff;
+}
+.secondary {
+  background-color: #6c757d;
+  color: #fff;
+}
+.tertiary {
+  background-color: #f8f9fa;
+  color: #212529;
+  border: 1px solid #ccc;
+}
+</style>
